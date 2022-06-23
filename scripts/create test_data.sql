45ebc89757113ef2b0a0b7ccb78ef4ad144e1808
stbe0616 <stepan.berendiaev@netcracker.com>
2022-06-23 10:24:05 +0400

    test2

--- scripts/create test_data.sql
+++ scripts/create test_data.sql
@@ -4,5 +4,9 @@ create table stb1
     id   serial primary key,
     name text not null unique
     --,mi_id integer REFERENCES menuitem(id) on delete cascade on update cascadeasdasda
+    --,mi_id integer REFERENCES menuitem(id) on delete cascade on update cascadeasdasda
+    --,mi_id integer REFERENCES menuitem(id) on delete cascade on update cascadeasdasda
+    --,mi_id integer REFERENCES menuitem(id) on delete cascade on update cascadeasdasda
+    --,mi_id integer REFERENCES menuitem(id) on delete cascade on update cascadeasdasda
 );
 
